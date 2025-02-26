e156fbad1b7403bb97708b147f1d3cecf18037c4
ATC-mmueller <mueller.marcus.go@gmail.com>
2025-02-26 12:44:42 +0100

    Changed the is_prime method to reflect the changed made to the markdown file

--- helper_functions/is_prime/src/lib.rs
+++ helper_functions/is_prime/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn is_prime(number: u64) -> bool {
     let upper_limit = (number as f64).sqrt().floor() as u64;
   
-    for factor in 2..upper_limit {
+    for factor in 2..upper_limit+1 {
       if factor == 2 || factor == 3 {
         if number % factor == 0 {
         return false;
